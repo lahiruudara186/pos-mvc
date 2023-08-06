e653ac1d46e8bbb37a5c16f6005469e512c559c5
lahiruudara186 <g12088.lahiru@gmail.com>
2023-08-06 18:53:28 +0530

    Create Customer Manage UI

--- src/pos/mvc/PosMvc.java
+++ src/pos/mvc/PosMvc.java
@@ -4,6 +4,8 @@
  */
 package pos.mvc;
 
+import pos.mvc.view.CustomerView;
+
 /**
  *
  * @author udara
@@ -14,7 +16,7 @@ public class PosMvc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new CustomerView().setVisible(true);
     }
     
 }
